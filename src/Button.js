e9f4e9734b7e5d518d582a1d162cde1c77c676fb
dev-jino <wlsgh2029@naver.com>
2023-09-26 10:40:47 +0900

    react v2.0

---
+++ src/Button.js
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+
+function Button({text}) {
+    return (
+        <button>
+            {text}
+        </button>
+    );
+  }
+
+  Button.propTypes = {
+    text : PropTypes.string
+  };
+  
+  export default Button;
+  
---
+++ src/Input.js
@@ -0,0 +1,8 @@
+function Input() {
+    return (
+        <input type="text" placeholder="값을 입력하세요!"/>        
+    );
+  }
+  
+  export default Input;
+  
